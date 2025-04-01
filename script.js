c6d7fe5cf2c3bfc2d84b8115adcb57eb8eef674c
CaioCesarFaria <321caioster@gmail.com>
2025-04-01 13:30:45 -0300

    Ok ate agora

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ function handleSwiper() {
 window.addEventListener('load', handleSwiper);
 window.addEventListener('resize', handleSwiper);
 
+let swiperDepoimentos;
+
+function initDepoimentosSwiper() {
+    if (window.innerWidth <= 768 && !swiperDepoimentos) {
+        swiperDepoimentos = new Swiper('.swiper_clientes', {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+    } else if (window.innerWidth > 768 && swiperDepoimentos) {
+        swiperDepoimentos.destroy(true, true);
+        swiperDepoimentos = undefined;
+    }
+}
+
+window.addEventListener('load', initDepoimentosSwiper);
+window.addEventListener('resize', initDepoimentosSwiper);
